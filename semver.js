0fb4246e93c311fae8c22818383812ac25763cda
Роман <blackwanted@yandex.ru>
2021-04-17 00:21:40 +0300

    Update semver.js

--- semver.js
+++ semver.js
@@ -1,14 +1,75 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
 let stdout = execSync(`git log -1 --pretty='%s'`).toString('utf-8');
-let commitName = stdout.slice(1, -2);
+let commitName = stdout
+  .slice(1, -2)
+  .split('--')
+  .map((x) => x.trim());
 
-if (commitName.match(/^--Patch /)) {
-  commitName = commitName.replace('--Patch ', '');
+let isUpdated = false;
+
+if (commitName[0] === 'Fix') {
   execSync(`npm version patch`).toString('utf-8');
-} else if (commitName.match(/^--Minor /)) {
-  commitName = commitName.replace('--Minor ', '');
+  isUpdated = true;
+} else if (commitName[0] === 'Add' || commitName[0] === 'Update') {
   execSync(`npm version minor`).toString('utf-8');
-} else if (commitName.match(/^--Major /)) {
-  commitName = commitName.replace('--Major ', '');
+  isUpdated = true;
+} else if (commitName[0] === 'Change' || commitName[0] === 'Release') {
   execSync(`npm version major`).toString('utf-8');
+  isUpdated = true;
+}
+
+if (!isUpdated) {
+  process.exit(1);
+}
+
+// Get current vesrion
+const version = JSON.parse(fs.readFileSync('package.json', 'utf-8'))['version'];
+
+// Update change log
+if (commitName.length > 1) {
+  // Changelog fill
+  let content = '';
+  try {
+    content = fs.readFileSync('CHANGELOG.md', 'utf-8');
+  } catch {
+    content = '';
+  }
+
+  let changeLog = `## [${version}] - ${new Date().getFullYear()}-${(
+    '00' + new Date().getMonth()
+  ).slice(-2)}-${('00' + new Date().getDate()).slice(-2)}
+
+`;
+
+  if (commitName[0] === 'Fix') {
+    changeLog += `### Fixed\n`;
+  }
+
+  if (commitName[0] === 'Add') {
+    changeLog += `### Added\n`;
+  }
+
+  if (commitName[0] === 'Update') {
+    changeLog += `### Updated\n`;
+  }
+
+  if (commitName[0] === 'Change') {
+    changeLog += `### Changed\n`;
+  }
+
+  if (commitName[0] === 'Release') {
+    changeLog += `### Release\n`;
+  }
+
+  changeLog += commitName
+    .slice(1)
+    .map((x) => `-    ${x}`)
+    .join('\n');
+  changeLog += '\n\n';
+
+  fs.writeFileSync(`CHANGELOG.md`, changeLog + content);
+
+  execSync(`git add CHANGELOG.md`).toString('utf-8');
+  execSync(`git commit -m "Changelog update"`).toString('utf-8');
 }
